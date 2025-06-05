3e7b2b82b93277658032dec418104591dc75fadc
Zlaxiu <bbanas797@gmail.com>
2025-06-05 20:35:55 +0200

    Added new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 import smLogisticImg from "../assets/projects/sm-logistic.png"
 import alpin from "../assets/projects/alpin.png"
-import knapik from "../assets/projects/knapik.png"
-
+import realizacje from "../assets/projects/realizacje.png"
 export const navLinks = [
     {
       id: "projects",
@@ -40,11 +39,11 @@ const projects = [
   },
   {
     id:3,
-    img:knapik,
-    title:"Kancelaria Adwokacka",
-    description:"A professional website for a law office, emphasizing trust and credibility through minimalist design and clear content structure.",
+    img:realizacje,
+    title:"Malé a Velké Rekonstrukce",
+    description:"A robust and trustworthy website for a renovation company, highlighting craftsmanship, reliability, and a clear service offer through a clean, professional layout.",
     tech:["cms","css"],
-    link:"https://www.skgkancelarie.pl/"
+    link:"https://male-a-velke-rekonstrukce.cz/"
   }
 ];
 const about = "Hey! I'm Bartek, a front-end developer focused on building modern, intuitive, and responsive user interfaces. I specialize in crafting clean and performant websites using technologies like React, Tailwind CSS, and modern JavaScript. Whether it's turning a static design into a dynamic experience or experimenting with smooth animations and interactions — I’m all about delivering thoughtful and polished solutions. I’m constantly learning, always curious, and genuinely enjoy solving problems through code. My approach combines a strong visual sense with a deep respect for usability and performance. Outside of coding, I love exploring new ideas, tinkering with visuals, and pushing my skills with each new project."
